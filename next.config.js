2ec60b2394b520020a2800bdc97d43f35d0e4586
Livingstone Maghanga <stoneriga@gmail.com>
2023-05-24 11:24:06 +0300

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+//   // trailingSlash: true,
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// };
+
 module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId },
+  ) {
+    
+  },
   reactStrictMode: true,
   images:{
+    unoptimized: true,
     domains: ["links.papareact.com", "image.tmdb.org"],
   },
 };
+
+
+
+
