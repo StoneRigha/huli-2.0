4ef50a6826db786ea8aef40dde16ef14040aa9f5
Livingstone Maghanga <stoneriga@gmail.com>
2023-07-14 15:45:16 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,36 +1,49 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: "export",
-  exportPathMap: async function (
-    defaultPathMap,
-    {dev,dir, outDir, distDir, buildId}
-    ){
-      return{
-        '/':{page: '/'}
-      }
-    },
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: "export",
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     {dev,dir, outDir, distDir, buildId}
+//     ){
+//       return{
+//         '/':{page: '/'}
+//       }
+//     },
+//   images: {
+//     loader: "akamai",
+//     path: "",
+//     unoptimized: "true",
+//     domains: ["links.papareact.com", "image.tmdb.org"],
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// module.export = nextConfig;
+
+// module.export = {
+//   exportPathMap: async function () {
+//     const paths = {
+//     "/": { page: "/" },
+//     };
+//     return paths;
+//     },
+// }
+
+module.exports = {
+  reactStrictMode: true,
+
   images: {
-    loader: "akamai",
-    path: "",
-    unoptimized: "true",
-    domains: ["links.papareact.com", "image.tmdb.org"],
+    domains: ['links.papareact.com','image.tmdb.org'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-};
-
-module.export = nextConfig;
-
-module.export = {
-  exportPathMap: async function () {
-    const paths = {
-    "/": { page: "/" },
-    };
-    return paths;
-    },
 }
